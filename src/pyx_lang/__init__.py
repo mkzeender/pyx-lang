b6760a86d4929fda3d37837f0b7840437e2973a0
Marckie Zeender <mkzeender@gmail.com>
2025-04-26 22:52:53 -0600

    autoinstall

--- src/pyx_lang/__init__.py
+++ src/pyx_lang/__init__.py
@@ -2,10 +2,16 @@ from sys import meta_path
 
 from pyx_lang.importer.pyx_importer import pyx_importer
 
+_installed = False
 
 def install():
     """
-    Installs the import hook, allowing you to import pyx files. Typically, this is called automatically when Python starts
+    Installs the import hook, allowing you to import pyx files.
+    Typically, this is called automatically when Python starts.
     """
-    if pyx_importer not in meta_path:
+    global _installed
+    
+    if not _installed:
+        _installed = True
         meta_path.insert(0, pyx_importer)
+
--- src/pyx_lang/autoinstall.py
+++ src/pyx_lang/autoinstall.py
@@ -1,5 +1,6 @@
 """
-Importing this module automatically installs the pyx import hook
+Importing this module automatically installs the pyx import hook.
+This module is typically automatically imported when python loads.
 """
 
 from . import install
